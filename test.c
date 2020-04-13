f53c8b7a3392a5525f0b7091bd0a2158deb2011c
Francois Chabanne <fe.chabanne@gmail.com>
2020-04-13 16:25:32 +0200

    completed

--- test.c
+++ test.c
@@ -20,8 +20,8 @@ int main(void)
 	//printf("%-10.5s\n", "Hello World");
 	//ft_printf("salut%-10c\ncoucou%5c\ntoi", 'A', 'B');
 	//printf("salut%-10c\ncoucou%5c\ntoi", 'A', 'B');
-	ft_printf("%010.4d\n", 42);
-	printf("%010.4d\n", 42);
+	ft_printf("%c", '\0');
+	printf("%5c", '\0');
 	//printf("%s\n", ft_uhex(-1 + 1 + 4294967295, 'x'));
      return (0);
 }
