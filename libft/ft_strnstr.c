09cd4440560c4ed68ff41d3d0c63361882d88a48
Francois Chabanne <fe.chabanne@gmail.com>
2020-04-30 11:31:18 +0200

    final commit

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: frchaban <frchaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 12:24:20 by Francois          #+#    #+#             */
-/*   Updated: 2020/04/28 16:14:22 by frchaban         ###   ########.fr       */
+/*   Updated: 2020/04/29 15:59:56 by frchaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	unsigned int j;
 
 	i = 0;
-	if (*needle == '\0')
-		return ((char*)haystack);
+	if (!haystack)
+		return (NULL);
+	if (needle == NULL || *needle == '\0')
+		return ((char *)haystack);
 	while (haystack[i] && i < len)
 	{
 		j = 0;
 		while (haystack[i + j] == needle[j] && i + j < len)
 		{
 			if (needle[j + 1] == '\0')
-				return (&((char*)haystack)[i]);
+				return ((char *)haystack + i);
 			j++;
 		}
 		i++;
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: frchaban <frchaban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/12 19:35:45 by Francois          #+#    #+#             */
-/*   Updated: 2020/04/28 15:59:13 by frchaban         ###   ########.fr       */
+/*   Updated: 2020/04/29 15:16:56 by frchaban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	int		i;
+	size_t	s_len;
 
-	i = 0;
-	if (!s)
+	if (!s || start < 0)
 		return (NULL);
-	if (start > ft_strlen(s))
-		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		len = 0;
+	else
+		len = (s_len - start < len ? s_len - start : len);
 	if (!(str = ft_calloc(len + 1, sizeof(char))))
 		return (NULL);
+	i = 0;
 	while (s[start] && len)
 	{
 		str[i] = s[start];
